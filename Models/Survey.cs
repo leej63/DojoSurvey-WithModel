0c9c8cb7c12be514c4682d2e70c82cc2a011b197
Jonathan <jonathan_496@hotmail.com>
2019-10-04 15:59:13 -0700

    first commit

---
+++ Models/Survey.cs
@@ -0,0 +1,22 @@
+namespace DojoSurveyWithModel.Models
+{
+    public class Survey
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Language { get; set; }
+        public string Comment { get; set; }
+
+        public Survey()
+        {
+            
+        }
+        public Survey(string name, string location, string language, string comment)
+        {
+            Name = name;
+            Location = location;
+            Language = language;
+            Comment = comment;
+        }
+    }
+}
